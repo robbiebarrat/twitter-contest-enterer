852be4e9cb4cae05bfeb085ea87b8c9f21b70a41
Alejandro <alec.augal@gmail.com>
2017-08-23 13:02:20 +0200

    Update twitterbot.py
    Made some changes with the script, it works exactly the same way, but with less code, making the algorithms simpler than before.

--- twitterbot.py
+++ twitterbot.py
@@ -33,29 +33,39 @@ def search(twts):
         if not any(k in i.text.lower() for k in keywords) or any(k in i.text.lower() for k in bannedwords):
             continue
         # Retweets
-        try:
+        #ORIGINAL CODE
+        #try:
+        #    api.retweet(i.id)
+        #    print "JUST RETWEETED " + (i.text)
+        #except:
+        #    print "Hm... Something went wrong.\nYou've probably already retweeted this."
+        #Checking before retweeting if the tweet was retweeted by the authenticated api account.
+        if not i.retweeted:
             api.retweet(i.id)
             print "JUST RETWEETED " + (i.text)
-        except:
-            print "Hm... Something went wrong.\nYou've probably already retweeted this."
         # Follows
         if "follow" in i.text or "Follow" in i.text or "FOLLOW" in i.text:
             # This part follows the actual contest-holder, instead of some random person who retweeted their contest
-            tweet = i.text
-            if tweet[0:3] == "RT ":
-                tweet = tweet[3:]
-            if tweet[0] == "@":
-                splittext = (tweet).split(":")
-                username = str(splittext[0]).replace("@", "")
-                api.create_friendship(username)
-                print "JUST FOLLOWED " + (username)
-            else:
+            #ORIGINAL CODE
+            #tweet = i.text
+            #if tweet[0:3] == "RT ":
+            #    tweet = tweet[3:]
+            #if tweet[0] == "@":
+            #    splittext = (tweet).split(":")
+            #    username = str(splittext[0]).replace("@", "")
+            #    api.create_friendship(username)
+            #    print "JUST FOLLOWED " + (username)
+            # This currently works, but you can do it easiest.
+            user_id = i.retweeted_status.user.id
+            api.create_friendship(user_id)
+        else:
                 username = i.user.screen_name
                 api.create_friendship(username)
                 print "JUST FOLLOWED " + str(username)
 
         # This next part favorites tweets if it has to
-        if "fav" in i.text or "Fav" in i.text or "FAV" in i.text:
+        # Added the check if the tweet was favorited before to avoid raising exceptions.
+        if ("fav" in i.text or "Fav" in i.text or "FAV" in i.text) and not i.favorited:
             api.create_favorite(i.id)
             print "JUST FAVORITED " + (i.text)
         # This part waits a minute before moving onto the next one.
